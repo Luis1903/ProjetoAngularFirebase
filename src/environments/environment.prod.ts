3a5434b30b2818a65cbc04eaffd4b3458d851fbf
Luis Felipe Santiago <santiagofelipe1903@gmail.com>
2025-02-19 12:30:15 -0300

    colocando o firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyCGdwCWx_8o2hYEmLBhK6xQzCMp5h12gzk",
-    authDomain: "aula-3e82a.firebaseapp.com",
-    projectId: "aula-3e82a",
-    storageBucket: "aula-3e82a.firebasestorage.app",
-    messagingSenderId: "258284807710",
-    appId: "1:258284807710:web:72b06b67cea8d15cd6b00f",
-    measurementId: "G-X8L8GS7897"
+    apiKey: "AIzaSyDlho4_k4LTRMtBzWsCpg7yerlqnrjJAto",
+    authDomain: "aula-2cc10.firebaseapp.com",
+    projectId: "aula-2cc10",
+    storageBucket: "aula-2cc10.firebasestorage.app",
+    messagingSenderId: "316333034176",
+    appId: "1:316333034176:web:5e8ffb2a461b2700f0bb25",
+    measurementId: "G-YSMV3DY6JM"
   },
   production: true
 };
